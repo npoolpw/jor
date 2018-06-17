9e2091dbc4b0d4ea0e4468df318d45d1e4a6d57d
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-17 10:11:27 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = "https://api.npool.pw/jor";
+
+var poolHost = "ca.npool.pw";
+
+var blockExplorer = "#";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var blockchainExplorer = "#?hash={id}#blockchain_block";
+
+var transactionExplorer = "#?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["npool.pw/jor", "https://api.npool.pw/jor"],
+        ["pool.jormo.org", "https://pool.jormo.org/api"]
+];
